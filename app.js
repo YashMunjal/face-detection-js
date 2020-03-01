7d66f5e2fb9429ad2f94bc37b8df3bc6a247220f
YashMunjal <yashmu45@gmail.com>
2020-03-01 23:06:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ navigator.msGetUserMedia
 || navigator.mozGetUserMedia;
 
 const modelParams = {
-    flipHorizontal: false, // flip e.g for video
-    imageScaleFactor: 0.7, // reduce input image size for gains in speed.
+    flipHorizontal: true, // flip e.g for video
+    imageScaleFactor: 0.2, // reduce input image size for gains in speed.
     maxNumBoxes: 10, // maximum number of boxes to detect
     iouThreshold: 0.5, // ioU threshold for non-max suppression
     scoreThreshold: 0.89 // confidence threshold for predictions.
@@ -35,7 +35,14 @@ function runDetection(){
             let handused = predictions[0].bbox;
             let x = handused[0];
             let y = handused[1];
-            window.scrollTo(x,y);
+            
+            window.scrollTo(x,y*5);
+            
+        }
+        else
+        {
+            var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+            y=y+scrollTop;
         }
             
     });
